ddddc34665221dcfdf4ea8f248eb5732b4f6b9b8
Chris Lind & Robin Miller <student@epicodus.com>
2014-04-07 17:22:59 -0700

    errors at adding bus times for arrivals

--- app/controllers/arrivals_controller.rb
+++ app/controllers/arrivals_controller.rb
@@ -2,4 +2,20 @@ class ArrivalsController < ApplicationController
   def index
     @arrivals = Arrival.all
   end
+
+  def create
+    @arrival_times = params[:arrival_times]
+    {26 => '12:50', 26 => "1:30"}
+    @arrival_times.each do |id,at|
+      Arrival.create({:time => at, :stop_id => id, :bus_id => params[:bus_id]})
+    end
+    render :root
+  end
+
+  private
+    def arrival_params
+      params.require(:arrival).permit(:bus_id, :arrival_times)
+    end
+
+
 end
